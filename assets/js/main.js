35cfcdb21f34bfe45f9e98116f704f35cc5c740a
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-08-25 16:20:12 +0200

    Add
    stamp in page with style

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,40 @@ Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
  - Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 */
 
-
+const listEl = document.getElementById("list")
 
 //Inizio con la stampa dei numeri in console (se il numero è multiplo di tre stampo"Fizz" , se il numero è multiplo di 5 stampo "Buzz" se il numero è multiplo sia di 3 che di 5 stampo "FizzBuzz" )
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("Fizz Buzz");
+        const fizzBuzzReturn = "FizzBuzz"
+        console.log(fizzBuzzReturn)
+
+        //Stampo in pagina il risultato
+        const fizzBuzzCard = `<div class=col-2><div class="d-flex align-items-center justify-content-center fw-bold p-2 m-2 ">${fizzBuzzReturn}</div></div>`;
+        listEl.innerHTML += fizzBuzzCard;
     }
     else if (i % 3 == 0) {
-        console.log("Fizz");
+        const fizzBuzzReturn = "Fizz"
+        console.log(fizzBuzzReturn);
+
+        //Stampo in pagina il risultato
+        const fizzBuzzCard = `<div class=col-2><div class="d-flex align-items-center justify-content-center fw-bold p-2 m-2 ">${fizzBuzzReturn}</div></div>`;
+        listEl.innerHTML += fizzBuzzCard;
     } else if (i % 5 == 0) {
-        console.log("Buzz");
+        const fizzBuzzReturn = "Buzz"
+        console.log(fizzBuzzReturn);
+
+        //Stampo in pagina il risultato
+        const fizzBuzzCard = `<div class=col-2><div class="d-flex align-items-center justify-content-center fw-bold p-2 m-2 ">${fizzBuzzReturn}</div></div>`;
+        listEl.innerHTML += fizzBuzzCard;
     } else {
-        console.log(i);
+        const fizzBuzzReturn = i;
+        console.log(fizzBuzzReturn);
+
+        //Stampo in pagina il risultato
+        const fizzBuzzCard = `<div class=col-2><div class="d-flex align-items-center justify-content-center fw-bold p-2 m-2 ">${fizzBuzzReturn}</div></div>`;
+        listEl.innerHTML += fizzBuzzCard;
+
+
     }
 }
